8cd9d6408219ae9f4e32a7b02730856396bb15f0
Sam <sam@sarocu.com>
2020-05-14 19:03:50 -0600

    feat: scaffolding csv loader

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,9 @@ mod utils;
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
-use std::io;
-
+use std;
 extern crate csv;
-use csv::{Reader};
+use csv::{Position, Reader};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -21,22 +20,28 @@ cfg_if! {
 }
 
 impl CsvFile {
-    pub fn new(
-        file_path: String
-    ) -> CsvFile {        
+    pub fn new(file_path: String) -> CsvFile {
         let new_csv = CsvFile {
             csv_file: file_path.to_owned(),
         };
-        
+
         new_csv
     }
 
-    // pub fn get(
-    //     skip: i32,
-    //     limit: i32,
-    // ) {
-    //     // return a slice of the csv file
-    // }
+    pub fn get(&self, skip: u64, limit: usize) -> std::vec::Vec<csv::StringRecord> {
+        // return a slice of the csv file
+        let mut csv_reader = Reader::from_path(self.csv_file.to_owned()).unwrap();
+        let item_count = csv_reader.headers().iter().count();
+        let start = Position::new().set_line(skip).to_owned();
+        csv_reader.seek(start.clone());
+        let mut rows = vec![csv::StringRecord::default()];
+        for index in 1..limit {
+            if let Some(row) = csv_reader.records().next() {
+                rows.push(row.unwrap())
+            }
+        }
+        return rows;
+    }
 
     // pub fn count(&self) -> Result<usize, ()> {
     //     // return the total rows
